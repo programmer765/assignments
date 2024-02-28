f2a470d89a44321eddd123f6a0c5d8da1e582bb1
Aviprit Shah <avipritshah15@gmail.com>
2024-02-29 02:49:02 +0530

    done 2 easy tasks

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,14 @@
 */
 
 function isAnagram(str1, str2) {
-
+  const freqArr = {}
+  for(const char of str1) freqArr[char.toLowerCase()] = (freqArr[char.toLowerCase()] || 0) + 1;
+  for(const char of str2) freqArr[char.toLowerCase()] = (freqArr[char.toLowerCase()] || 0) - 1;
+  console.log(freqArr)
+  for(char in freqArr) {
+    if(freqArr[char]) return false
+  }
+  return true
 }
 
 module.exports = isAnagram;
